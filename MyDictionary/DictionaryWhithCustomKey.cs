aa067679eb3317013e9a93ef1565b1445b3edb46
Aleksey Linnik <dom_istorika@mail.ru>
2017-12-30 19:45:04 +0700

    Полностью переработал словарь

---
+++ MyDictionary/DictionaryWhithCustomKey.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyDictionary
+{
+    public interface IMyKey<TId, TName>
+    {
+        TId Id { get; set; }
+        TName Name { get; set; }
+    }
+
+    public class DictionaryWhithCustomKey<TId, TName, TValue> : IDictionary<IMyKey<TId, TName>, TValue>
+                                                        where TId : IEquatable<TId>
+                                                        where TName : IEquatable<TName>
+                                                        where TValue : IEquatable<TValue>
+    {
+        private List<MyObject<TId, TName, TValue>> values;
+
+        public DictionaryWhithCustomKey()
+        {
+            this.values = new List<MyObject<TId, TName, TValue>>();
+        }
+
+        public TValue this[IMyKey<TId, TName> key]
+        {
+            get
+            {
+                return GetValue(key);
+            }
+
+            set
+            {
+                SetValue(key, value);
+            }
+        }
+        public ICollection<TValue> this[TId id] => GetValue(id);
+
+        public ICollection<TValue> this[int key, TName name] => GetValue(name);
+
+        private TValue GetValue(IMyKey<TId, TName> key)
+        {
+            foreach (var myObject in values)
+            {
+                if (myObject.MyKey.Id.Equals(key.Id) && myObject.MyKey.Name.Equals(key.Name))
+                {
+                    return myObject.Value;
+                }
+            }
+            return default(TValue);
+        }
+        private ICollection<TValue> GetValue(TId id)
+        {
+            ICollection<TValue> collection = new List<TValue>();
+            foreach (var item in values)
+            {
+                if (item.MyKey.Id.Equals(id))
+                {
+                    collection.Add(item.Value);
+                }
+            }
+            return collection;
+        }
+        private ICollection<TValue> GetValue(TName name)
+        {
+            ICollection<TValue> collection = new List<TValue>();
+            foreach (var item in values)
+            {
+                if (item.MyKey.Name.Equals(name))
+                {
+                    collection.Add(item.Value);
+                }
+            }
+            return collection;
+        }
+
+        private Boolean SetValue(IMyKey<TId, TName> key, TValue value)
+        {
+            foreach (var item in values)
+            {
+                if (item.MyKey.Id.Equals(key.Id) && item.MyKey.Name.Equals(key.Name))
+                {
+                    item.Value = value;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        public int Count => values.Count;
+
+        public bool IsReadOnly => false;
+
+        public ICollection<IMyKey<TId, TName>> Keys
+        {
+            get
+            {
+                var myKeys = new List<IMyKey<TId, TName>>();
+                foreach (var myObject in values)
+                {
+                    myKeys.Add(myObject.MyKey);
+                }
+                return myKeys;
+            }
+        }
+
+        public ICollection<TValue> Values
+        {
+            get
+            {
+                var listValues = new List<TValue>();
+                foreach (var myObject in values)
+                {
+                    listValues.Add(myObject.Value);
+                }
+                return listValues;
+            }
+        }
+
+        public void Add(KeyValuePair<IMyKey<TId, TName>, TValue> item)
+        {
+            values.Add(new MyObject<TId, TName, TValue>(item.Key.Id, item.Key.Name, item.Value));
+        }
+
+        public void Add(IMyKey<TId, TName> key, TValue value)
+        {
+            values.Add(new MyObject<TId, TName, TValue>(key.Id, key.Name, value));
+        }
+
+        public void Clear()
+        {
+            values.Clear();
+        }
+
+        public bool Contains(KeyValuePair<IMyKey<TId, TName>, TValue> item)
+        {
+            foreach (var myObject in values)
+            {
+                if (myObject.MyKey.Id.Equals(item.Key.Id)
+                    && myObject.MyKey.Name.Equals(item.Key.Name)
+                    && myObject.Value.Equals(item.Value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool ContainsKey(IMyKey<TId, TName> key)
+        {
+            foreach (var myObject in values)
+            {
+                if (myObject.MyKey.Equals(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void CopyTo(KeyValuePair<IMyKey<TId, TName>, TValue>[] array, int arrayIndex)
+        {
+            for (int i = arrayIndex; i < array.Length; i++)
+            {
+                array[i] = new KeyValuePair<IMyKey<TId, TName>, TValue>(values[i].MyKey, values[i].Value);
+            }
+        }
+
+        public IEnumerator<KeyValuePair<IMyKey<TId, TName>, TValue>> GetEnumerator()
+        {
+            return new DictionaryWhithCustomKeyEnumerator<TId, TName, TValue>(this);
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new DictionaryWhithCustomKeyEnumerator<TId, TName, TValue>(this);
+        }
+
+        public bool Remove(KeyValuePair<IMyKey<TId, TName>, TValue> item)
+        {
+            foreach (var myObject in values)
+            {
+                if (myObject.MyKey.Id.Equals(item.Key.Id)
+                    && myObject.MyKey.Name.Equals(item.Key.Name)
+                    && myObject.Value.Equals(item.Value))
+                {
+                    values.Remove(myObject);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool Remove(IMyKey<TId, TName> key)
+        {
+            foreach (var myObject in values)
+            {
+                if (myObject.MyKey.Id.Equals(key.Id)
+                    && myObject.MyKey.Name.Equals(key.Name))
+                {
+                    values.Remove(myObject);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool TryGetValue(IMyKey<TId, TName> key, out TValue value)
+        {
+            foreach (var myObject in values)
+            {
+                if (myObject.MyKey.Id.Equals(key.Id)
+                    && myObject.MyKey.Name.Equals(key.Name))
+                {
+                    value = myObject.Value;
+                    return true;
+                }
+            }
+            value = default(TValue);
+            return false;
+        }
+
+
+
+        #region MyObject
+
+        private class MyObject<TId1, TName1, TValue1>
+        {
+            public class Key : IMyKey<TId1, TName1>
+            {
+                public TId1 Id { get; set; }
+                public TName1 Name { get; set; }
+            }
+
+            public readonly Key MyKey;
+            public TValue1 Value { get; set; }
+            public MyObject(TId1 id, TName1 name, TValue1 value)
+            {
+                MyKey = new Key
+                {
+                    Id = id,
+                    Name = name
+                };
+                Value = value;
+            }
+        }
+
+        #endregion
+
+        #region DictionaryWhithCustomKeyEnumerator
+
+        private class DictionaryWhithCustomKeyEnumerator<TId1, TName1, TValue1> :IEnumerator<KeyValuePair<IMyKey<TId1, TName1>, TValue1>> 
+            where TId1 : IEquatable<TId1>
+            where TName1 : IEquatable<TName1>
+            where TValue1 : IEquatable<TValue1>
+        {
+            private List<DictionaryWhithCustomKey<TId1, TName1, TValue1>.MyObject<TId1, TName1, TValue1>> values;
+            private Int32 _curretIndex = -1;
+
+            public DictionaryWhithCustomKeyEnumerator(DictionaryWhithCustomKey<TId1, TName1, TValue1> dictionary)
+
+            {
+                this.values = dictionary.values;
+            }
+            public object Current
+            {
+                get
+                {
+                    ValidateIndex();
+                    return values[_curretIndex];
+                }
+            }
+
+            KeyValuePair<IMyKey<TId1, TName1>, TValue1> IEnumerator<KeyValuePair<IMyKey<TId1, TName1>, TValue1>>.Current
+            {
+                get
+                {
+                    IMyKey<TId1, TName1> key = (IMyKey<TId1, TName1>)values[_curretIndex].MyKey;
+                    TValue1 value = values[_curretIndex].Value;
+                    return new KeyValuePair<IMyKey<TId1, TName1>, TValue1>(key,value);
+                }
+            }
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                if (_curretIndex < values.Count - 1) { _curretIndex++; return true; }
+                return false;
+                
+            }
+
+            public void Reset()
+            {
+                _curretIndex = -1;
+            }
+
+            private void ValidateIndex()
+            {
+                if (_curretIndex < 0 || _curretIndex >= values.Count)
+                    throw new InvalidOperationException("Enumerator is before or after the collection");
+            }
+
+        }
+        
+
+        #endregion    }
+
+
+}
--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -7,89 +7,32 @@ using System.Threading;
 
 namespace MyDictionary
 {
-    using Collection = MyCollection<UserType<ID, string, string>, ID, string>;
-    using User = UserType<ID, string, string>;
-    class UserType<TId, TName, TValue> : IMyKey<TId, TName>
+    class MyKey : IMyKey<int, string>
     {
-        TId id;
-        TName name;
-        TValue value;
-        public UserType(TId id, TName name, TValue value)
-        {
-            this.id = id;
-            this.name = name;
-            this.value = value;
-        }
-        public TId Id
-        {
-            get{ return id;}
-        }
-        public TName Name
-        {
-            get { return name; }
-        }
-        public TValue Value
-        {
-            get { return value; }
-        }
+        public int Id { get; set; }
 
+        public string Name { get; set; }
+     
     }
-    class ID:IEquatable<ID>
-    {
-        public int ID1 { get; set; }
-        public int ID2 { get; set; }
-        public ID(int id1, int id2)
-        {
-            ID1 = id1;
-            ID2 = id2;
-        }
 
-        public override bool Equals(object obj)
-        {
-            if (obj is ID)
-                return Equals((ID)obj);
-            return false;
-
-        }
-        public bool Equals(ID other)
-        {
-            if (ID1 == other.ID1 && ID2 == other.ID2)
-                return true;
-            else
-                return false;
-        }
-        public override int GetHashCode()
-        {
-            return ID1 ^ ID2;
-        }
-    }    
-      
     class Program
     {
        
         static void Main(string[] args)
         {
-            User user1 = new User(new ID(10,13), "Name1","Value1");
-            User user2 = new User(new ID(2,211), "Name2","Value2");
-            Collection myCollection = new Collection();
-            
-            Thread thread1 = new Thread(myCollection.Add);
-            Thread thread2 = new Thread(myCollection.Add);
-            thread1.Start(user1);
-            thread2.Start(user2);
-           
-            thread1.Join();
-            thread2.Join();
-
-            ID findID = new ID(10, 13);
-            UserType<ID, string, string> userTemp = myCollection.Find(findID);
-            Console.WriteLine(userTemp.Id.ID1+ " " + userTemp.Id.ID2 + " " + userTemp.Name);
-            userTemp = myCollection.Find("Name2");
-            Console.WriteLine(userTemp.Id.ID1 + " " + userTemp.Id.ID2 + " " + userTemp.Name);
-            foreach (KeyValuePair<Collection.Key,User> dic in myCollection)
+            var myDictionay = new DictionaryWhithCustomKey<int, string, int>
             {
-                Console.WriteLine("Id = ({0},{1}); Name = {2}; Value = {3}",dic.Key.Id.ID1, dic.Key.Id.ID2, dic.Key.Name,dic.Value.Value);
+                {new MyKey() {Id = 1, Name = "Вася"}, 23},
+                {new MyKey() {Id = 1, Name = "Петя"}, 23},
+                {new MyKey() {Id = 2, Name = "Коля"}, 30},
+                {new MyKey() {Id = 3, Name = "Саша"}, 33},
+                {new MyKey() {Id = 4, Name = "Женя"}, 35}
+            };
+            foreach (var item in myDictionay)
+            {
+                Console.WriteLine($"Id:{item.Key.Id} Имя:{item.Key.Name} Возраст:{item.Value}");
             }
+          
             Console.ReadKey();
         }
     }
